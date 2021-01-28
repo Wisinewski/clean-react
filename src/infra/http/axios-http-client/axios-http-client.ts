c9a70cee56c369ece2c593a4a34fe2e02e3e7f77
Wisinewski <g_gomees@hotmail.com>
2021-01-28 20:09:28 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++
@@ -1,8 +0,0 @@
-import { HttpPostParams } from '@/data/protocols/http/http-post-client'
-import axios from 'axios'
-
-export class AxiosHttpClient {
-  async post (params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url, params.body)
-  }
-}
