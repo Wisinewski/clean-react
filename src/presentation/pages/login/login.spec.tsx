236a89550d4c8308782942c5243ca1238343f032
Wisinewski <g_gomees@hotmail.com>
2021-02-03 11:51:35 -0300

    test: ensure Login present valid state if password is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -48,11 +48,20 @@ describe('Login Component', () => {
 
   test('should show password error if Validation fails', () => {
     const { sut, validationSpy } = makeSut()
-
     const passwordInput = sut.getByTestId('password')
     fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
     const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.title).toBe(validationSpy.errorMessage)
     expect(passwordStatus.textContent).toBe('🔴')
+  })
+
+  test('should show valid password state if Validation succeeds', () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.errorMessage = null
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+    const passwordStatus = sut.getByTestId('password-status')
+    expect(passwordStatus.title).toBe('OK')
+    expect(passwordStatus.textContent).toBe('🔵')
   })
 })
